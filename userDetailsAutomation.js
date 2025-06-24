87ea2a7709d57f6bae82a5ced81e5244ca8cc379
Anish <koiralaanish46@gmail.com>
2025-06-24 19:20:25 +0545

    update codes

--- userDetailsAutomation.js
+++ userDetailsAutomation.js
@@ -1,2 +1,6 @@
 //This is the automation feature for user details from my branch
-//Changed the function name
+//Changed the function names
+
+//This is my current change in curuent branch
+
+//This is further code
